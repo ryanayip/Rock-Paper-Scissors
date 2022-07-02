8d086e204b0b2771711766043fdf9a3a2baff8eb
Ryan Yip <ryip94@yahoo.com>
2022-07-01 21:46:31 -0700

    Completed win counter for computer and human players.

--- game.js
+++ game.js
@@ -10,6 +10,9 @@ function computerPlay() {
         
 }
 
+let yourScore = 0
+let computerScore = 0
+let roundCounter = 0
 
 function playRound(playerSelection,computerSelection) {
 
@@ -19,8 +22,10 @@ function playRound(playerSelection,computerSelection) {
 
     //I Pick Rock
     if (playerSelection == "Rock" && computerSelection == "Paper") {
+        ++computerScore
         return "You Lose! Paper covers Rock.";
     } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        ++yourScore 
         return "You Win! Rock crushes Scissors.";
     } else if (playerSelection == "Rock" && computerSelection == "Rock") {
         return "You both picked Rock, so it's a tie. How Boring"; 
@@ -29,33 +34,77 @@ function playRound(playerSelection,computerSelection) {
       else if (playerSelection == "Paper" && computerSelection == "Paper") {
         return "You both picked Paper, so it's a tie.";
     } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        ++computerScore
         return "You Lose! Scissors slices through Paper.";
     } else if (playerSelection == "Paper" && computerSelection === "Rock") {
+        ++yourScore
         return "You Win! Paper covers Rock.";
     }
     //I Pick Scissors
       else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        ++yourScore
         return "You Win! Scissors slices through Paper.";
     } else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
         return "You both picked Scissors, so it's a tie!";
     } else {
+        ++computerScore
         return "You Lose! Rock crushes Scissors";
     }
 
+    
 }
 
 
 function game() {
 
-    playerSelection = prompt("Type Rock, Paper, or Scissors")
     computerSelection = computerPlay()
+    playerSelection = ""
+   
 
+    console.log("---------------------------")
+    console.log(`Round ${++roundCounter}`)
+    console.log(playRound(playerSelection, computerSelection))
+    console.log(`Your Score : ${yourScore}`)
+    console.log(`Computer Score : ${computerScore}`)
+
+
+    console.log("---------------------------")
+    console.log(`Round ${++roundCounter}`)
+    console.log(playRound(playerSelection, computerSelection))
+    console.log(`Your Score : ${yourScore}`)
+    console.log(`Computer Score : ${computerScore}`)
+
+
+    console.log("---------------------------")
+    console.log(`Round ${++roundCounter}`)
+    console.log(playRound(playerSelection, computerSelection))
+    console.log(`Your Score : ${yourScore}`)
+    console.log(`Computer Score : ${computerScore}`)
     
+    console.log("---------------------------")
+    console.log(`Round ${++roundCounter}`)
     console.log(playRound(playerSelection, computerSelection))
+    console.log(`Your Score : ${yourScore}`)
+    console.log(`Computer Score : ${computerScore}`)
+
+
+    console.log("---------------------------")
+    console.log(`Round ${++roundCounter}`)
     console.log(playRound(playerSelection, computerSelection))
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(playRound(playerSelection, computerSelection))
+    console.log(`Your Score : ${yourScore}`)
+    console.log(`Computer Score : ${computerScore}`)
+    console.log("---------------------------")
+
+    if (yourScore > computerScore) {
+        console.log("Congratulations, you win!")
+    } else if (computerScore > yourScore) {
+        console.log("You lost. Too bad.")
+    } else {
+        console.log("Wow, a tie!")
+    }
+    console.log("---------------------------")
+
+
 
 
 
